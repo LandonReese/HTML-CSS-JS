d55009b9171853653307ea9ca07e4756bbbaf1b5
Landon Reese <landon.j.reese@gmail.com>
2023-02-23 11:21:04 -0600

    Feb 23rd Updates

--- script1.js
+++ script1.js
@@ -2,3 +2,5 @@ function update1() {
     var sheet = document.getElementById('stylesheet');
     sheet.setAttribute('href', 'style1.css');
 }
+
+//<!-- <button onclick = "document.getElementById('stylesheet').setAttribute('href', 'style1.css')">Link 2</button> -->
