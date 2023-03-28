c056991b5a972bf9ae81d0471056b7e1af8aa264
Landon Reese <landon.j.reese@gmail.com>
2023-03-28 00:07:55 -0500

    Update helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-    res.send("Hello World!");
-});
+
+function sayHello(res) {
+    res.send("Hello world!");
+}
+
+
+app.get('/', (req, res) => {sayHello(res)})
+// app.get('/', function(req, res){ //when you do a get request to this path '/', do the send command
+//     res.send("Hello World!");
+// });
 
 app.listen(3000);
