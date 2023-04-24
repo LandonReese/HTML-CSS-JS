fb36bbcf289891a662cec4ec6da43a9a432efb65
Landon Reese <landon.j.reese@gmail.com>
2023-04-23 21:56:28 -0500

    Updated Code

--- TO-DO/3_MongoDB/ExpressMongoDB.js
+++ TO-DO/3_MongoDB/ExpressMongoDB.js
@@ -8,6 +8,7 @@ const PORT = 3000;
 const app = express();
 
 mongoose.connect('mongodb://localhost/myapp', { useNewUrlParser: true, useUnifiedTopology: true });
+const uri = 'mongodb+srv://landonjreese:<password>@cluster0.gexe2rb.mongodb.net/?retryWrites=true&w=majority';
 
 app.use(express.json()); // enable JSON body parsing
 
@@ -121,55 +122,60 @@ let books = [
       who: "",
       due: ""
     },
+    {
+      id: "2",
+      title: "Express-sions",
+      author: "Frieda Livery",
+      publisher: "Chaotic House",
+      isbn: "978-3-16-148410-2",
+      avail: true,
+      who: "",
+      due: ""
+    },
+    {
+      id: "3",
+      title: "Restful REST",
+      author: "Al Gorithm",
+      publisher: "ACM",
+      isbn: "978-3-16-143310-1",
+      avail: true,
+      who: "",
+      due: ""
+    },
+    {
+      id: "4",
+      title: "See Essess",
+      author: "Anna Log",
+      publisher: "O'Reilly",
+      isbn: "987-6-54-148220-1",
+      avail: false,
+      who: "Homer",
+      due: "1/1/23"
+    },
+    {
+      id: "5",
+      title: "Scripting in JS",
+      author: "Dee Gital",
+      publisher: "IEEE",
+      isbn: "987-6-54-321123-1",
+      avail: false,
+      who: "Marge",
+      due: "1/2/23"
+    },
+    {
+      id: "6",
+      title: "Be An HTML Hero",
+      author: "Jen Neric",
+      publisher: "Coders-R-Us",
+      isbn: "987-6-54-321123-2",
+      avail: false,
+      who: "Lisa",
+      due: "1/3/23"
+    }
 ];
 
-//   {
-//     id: "2",
-//     title: "Express-sions",
-//     author: "Frieda Livery",
-//     publisher: "Chaotic House",
-//     isbn: "978-3-16-148410-2",
-//     avail: true,
-//     who: "",
-//     due: ""
-//   },
-//   {
-//     id: "3",
-//     title: "Restful REST",
-//     author: "Al Gorithm",
-//     publisher: "ACM",
-//     isbn: "978-3-16-143310-1",
-//     avail: true,
-//     who: "",
-//     due: ""
-//   },
-//   {
-//     id: "4",
-//     title: "See Essess",
-//     author: "Anna Log",
-//     publisher: "O'Reilly",
-//     isbn: "987-6-54-148220-1",
-//     avail: false,
-//     who: "Homer",
-//     due: "1/1/23"
-//   },
-//   {
-//     id: "5",
-//     title: "Scripting in JS",
-//     author: "Dee Gital",
-//     publisher: "IEEE",
-//     isbn: "987-6-54-321123-1",
-//     avail: false,
-//     who: "Marge",
-//     due: "1/2/23"
-//   },
-//   {
-//     id: "6",
-//     title: "Be An HTML Hero",
-//     author: "Jen Neric",
-//     publisher: "Coders-R-Us",
-//     isbn: "987-6-54-321123-2",
-//     avail: false,
-//     who: "Lisa",
-//     due: "1/3/23"
-//   }
+// Put all initialized books into MongoDB
+for (let bookData of books) {
+    const book = new Book(bookData);
+    await book.save();
+}
